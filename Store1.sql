f22e6427f8489ef20071d659a12e962eb655f4a8
tanveermarne20 <>
2024-01-30 22:06:57 +0530

    Initial

--- Store1.sql
+++ Store1.sql
@@ -1,1 +1,23 @@
---This is main branch
+----------------Exploratory Data Analysis-------------------------------------
+--Generic Questions
+
+--1.Display distinct age_group.
+
+
+--2.Display distinct category 
+
+
+--3.Display customers who have purchased Kurta
+
+
+
+--4.Display details of  Teenagers  from karnataka.
+
+
+
+
+
+
+--5.Find category that has been shiped to New Delhi
+
+
