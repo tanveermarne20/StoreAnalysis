31ab5f7c45191000094f650c81d4729ce7bd953e
Kiransaste <>
2024-01-31 14:58:07 +0530

    second

--- Store1.sql
+++ Store1.sql
@@ -2,24 +2,28 @@
 --Generic Questions
 
 --1.Display distinct age_group.
-
+select distinct(age_group)
+from store;
 
 --2.Display distinct category 
+select distinct(category) from store;
 
 
 --3.Display customers who have purchased Kurta
-
+select cust_id,category from store
+where upper(category)='KURTA';
 
 
 --4.Display details of  Teenagers  from karnataka.
-
+select * from store where lower(age_group)='teenager';
 
 
 
 
 
 --5.Find category that has been shiped to New Delhi
-
+select category,ship_city
+from store where lower(ship_city)='new delhi';
 
 
 --6.Show total number of columns  in Store table
